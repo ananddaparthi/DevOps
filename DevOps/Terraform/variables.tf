242c3feb51825c329afb9ce2586a9cd25518167e
ananddaparthi <anand.daparthi1@gmail.com>
2017-10-23 01:47:20 -0400

    Update variables.tf

--- DevOps/Terraform/variables.tf
+++ DevOps/Terraform/variables.tf
@@ -1,12 +1,11 @@
 variable "aws_access_key" {
   description = "aws access key"
   default = "AKI********"
-  
-}
+  }
 variable "aws_secret_key" {
   description = "aws secret key"
   default = "bP5C***********"
-  
+  }  
 variable "aws_region" {
   description = "The AWS region to Launch my Instance"
   default     = "us-west-2"
